7d43ee353191583d8013ecc43faa98b6d2e681f9
IBatsios <ioannisbatsios@gmail.com>
2020-07-06 22:54:51 -0400

    adding 1 to the middle did not change the index, but resulted in an error to the right of the middle

--- Algorithms/binarysearch.js
+++ Algorithms/binarysearch.js
@@ -6,8 +6,7 @@ function binarySearch(array, itemToSearch) {
     const middle = left + Math.floor((right - left) / 2);
 
     if (array[middle] === itemToSearch) {
-      // adding one to the middle so first index is 1 and not 0
-      return middle + 1;
+      return middle;
     }
 
     if (array[middle] < itemToSearch) {
